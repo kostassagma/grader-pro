13f99e4c84c8165ee961fcf5005b2c4d9cafd8b7
Kostas <kostassagmatopoulos@gmail.com>
2025-03-07 12:41:49 +0200

    tos

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -7,6 +7,17 @@ import { useNavigate } from "react-router";
 import { useState } from "react";
 import { API } from "@/lib/constants";
 import { useAuthStore } from "@/lib/auth-store";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export function LoginForm({
   className,
@@ -18,99 +29,118 @@ export function LoginForm({
   const { setAutCredentials } = useAuthStore();
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Welcome back</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              const url = `${API}/login`;
-              const options = {
-                method: "POST",
-                headers: {
-                  Accept: "application/json",
-                  "Content-Type": "application/json;charset=UTF-8",
-                },
-                body: JSON.stringify({
-                  username,
-                  password,
-                }),
-              };
+    <AlertDialog>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Omerta</AlertDialogTitle>
+          <AlertDialogDescription>
+            In order to use this hack, you have to comply with the omerta
+            agreement:
+            <br /> I don't see, I don't hear, I don't speak.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <a href="https://www.google.com/search?q=snitch&oq=snitch&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDINCAEQLhivARjHARiABDIHCAIQABiABDIHCAMQABiABDIHCAQQLhiABDIHCAUQLhiABDIYCAYQLhgKGK8BGMcBGIsDGKYDGKgDGIAEMgwIBxAAGAoYiwMYgAQyBwgIEAAYjwIyBwgJEAAYjwLSAQc5NDZqMWo3qAIAsAIA&sourceid=chrome&ie=UTF-8">
+            <AlertDialogCancel>I am snitch</AlertDialogCancel>
+          </a>
+          <AlertDialogAction>I Agree</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Welcome back</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                const url = `${API}/login`;
+                const options = {
+                  method: "POST",
+                  headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json;charset=UTF-8",
+                  },
+                  body: JSON.stringify({
+                    username,
+                    password,
+                  }),
+                };
 
-              fetch(url, options)
-                .then((response) => {
-                  if (response.ok) {
-                    return response.json();
-                  } else {
-                    throw new Error("");
-                  }
-                })
-                .then((data) => {
-                  console.log(data);
-                  setAutCredentials(data.encrypted_username, data.encrypted_password, data.cookie);
-                  navigate("/app")
-                })
-                .catch((_err) => {
-                  console.log("Shit went wrong!");
-                });
-            }}
-          >
-            <div className="grid gap-6">
-              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                <span className="bg-background text-muted-foreground relative z-10 px-2">
-                  Fill your grader credentials
-                </span>
-              </div>
+                fetch(url, options)
+                  .then((response) => {
+                    if (response.ok) {
+                      return response.json();
+                    } else {
+                      throw new Error("");
+                    }
+                  })
+                  .then((data) => {
+                    console.log(data);
+                    setAutCredentials(
+                      data.encrypted_username,
+                      data.encrypted_password,
+                      data.cookie
+                    );
+                    navigate("/app");
+                  })
+                  .catch((_err) => {
+                    console.log("Shit went wrong!");
+                  });
+              }}
+            >
               <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="username">Username</Label>
-                  <Input
-                    id="username"
-                    type="username"
-                    placeholder="pi..."
-                    value={username}
-                    onChange={(e) => {
-                      e.preventDefault();
-                      setUsername(e.target.value);
-                    }}
-                    required
-                  />
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-background text-muted-foreground relative z-10 px-2">
+                    Fill your grader credentials
+                  </span>
                 </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="username">Username</Label>
+                    <Input
+                      id="username"
+                      type="username"
+                      placeholder="pi..."
+                      value={username}
+                      onChange={(e) => {
+                        e.preventDefault();
+                        setUsername(e.target.value);
+                      }}
+                      required
+                    />
                   </div>
-                  <Input
-                    id="password"
-                    type="password"
-                    required
-                    value={password}
-                    onChange={(e) => {
-                      e.preventDefault();
-                      setPassword(e.target.value);
-                    }}
-                  />
+                  <div className="grid gap-3">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input
+                      id="password"
+                      type="password"
+                      required
+                      value={password}
+                      onChange={(e) => {
+                        e.preventDefault();
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
                 </div>
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
               </div>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>
+            </form>
+          </CardContent>
+        </Card>
+        <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+          By clicking continue, you agree to our{" "}
+          <AlertDialogTrigger className="underline hover:cursor-pointer">
+            Terms of Service
+          </AlertDialogTrigger>
+        </div>
       </div>
-    </div>
+    </AlertDialog>
   );
 }
